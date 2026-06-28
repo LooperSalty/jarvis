7cf83eeeef5ed2f2a1fec65b75945ae3a832adac
LooperSalty <paulruiz944@gmail.com>
2026-06-28 15:01:19 +0200

    chore: bump version 0.3.1

--- jarvis_core/jarvis_version.py
+++ jarvis_core/jarvis_version.py
@@ -11,7 +11,7 @@ import json
 import urllib.request
 
 # Version courante. A bumper en meme temps que le tag git de release.
-VERSION = "0.3.0"
+VERSION = "0.3.1"
 
 # Depot GitHub (owner/repo) pour la verification de mise a jour.
 GITHUB_REPO = "LooperSalty/jarvis"
